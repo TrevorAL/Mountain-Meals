a7d997b0aed52141f0d1dbc7dac75103a9e463cd
Ronald Laukaitis <rol226@lehigh.edu>
2024-10-19 20:25:37 -0400

    basic front end

--- mountainmeals/src/pages/Home.jsx
+++ mountainmeals/src/pages/Home.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 
-const Home = () => (
-  <div>
-    <div className='about-panel'>
-    <h2>Welcome to MountainMeals!</h2>
-    <p>Please input the amount of meal swipes you have left for the semester.</p>
+const Home = () => {
+  const [mealSwipes, setMealSwipes] = useState('');
+
+  const handleInputChange = (event) => {
+    setMealSwipes(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`You have ${mealSwipes} meal swipes left for the semester.`);
+  };
+
+  return (
+    <div>
+      <div className='about-panel'>
+        <p>Enter your meal swipes to get started</p>
+        <div className='form'>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            value={mealSwipes}
+            onChange={handleInputChange}
+            placeholder="Enter meal swipes"
+            className="meal-input"
+          />
+          <button type="submit">Submit</button>
+        </form>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
